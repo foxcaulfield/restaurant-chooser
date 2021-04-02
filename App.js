e3801ffd9d4968a92bd733e231c59fe61cfc5235
Fox Caulfield <mrcoderprog@gmail.com>
2021-04-02 21:23:48 +0300

    Update navigation

--- App.js
+++ App.js
@@ -1,27 +1,16 @@
-import React, { useState } from "react";
-import {
-  Text,
-  View,
-  Button,
-  StyleSheet,
-  Platform,
-  SafeAreaView,
-  StatusBar,
-} from "react-native";
+// //================================================================================
+// // You can import Ionicons from @expo/vector-icons if you use Expo or
+// // react-native-vector-icons/Ionicons otherwise.
 
-import PeopleScreen from "./app/screens/PeopleScreen";
-import DecisionScreen from "./app/screens/DecisionScreen";
-import RestaurantsScreen from "./app/screens/RestaurantsScreen";
+// //
+// import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+// //
 
-import { Constants } from "expo";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-
-console.log("------------------------------------------------------------");
-console.log(`RestaurantChooser starting on ${Platform.OS}`);
-
-const platformOS = Platform.OS.toLowerCase();
+// import * as React from "react";
+// import { Text, View, Image } from "react-native";
+// import { Ionicons } from "@expo/vector-icons";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { NavigationContainer } from "@react-navigation/native";
 
 // function HomeScreen() {
 //   return (
@@ -39,17 +28,149 @@ const platformOS = Platform.OS.toLowerCase();
 //   );
 // }
 
+// const Tab = createBottomTabNavigator();
+
+// //
+// // const Tab = createMaterialTopTabNavigator();
+// //
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator
+//         screenOptions={({ route }) => ({
+//           tabBarIcon: ({ focused, color, size }) => {
+//             // You can return any component that you like here!
+//             return (
+//               <Image
+//                 source={require("./app/images/icon-people.png")}
+//                 style={{ width: 32, height: 32, tintColor: "black" }}
+//               />
+//             );
+//           },
+//         })}
+//         tabBarOptions={{
+//           activeTintColor: "tomato",
+//           inactiveTintColor: "gray",
+//         }}
+//       >
+//         <Tab.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           options={{
+//             tabBarLabel: "Home!",
+//             tabBarIcon: ({ focused, color, size }) => {
+//               return (
+//                 <Image
+//                   source={require("./app/images/icon-restaurants.png")}
+//                   style={{ width: 32, height: 32, tintColor: "black" }}
+//                 />
+//               );
+//             },
+//           }}
+//         />
+
+//         <Tab.Screen
+//           name="Settings"
+//           component={SettingsScreen}
+//           options={{
+//             tabBarLabel: "12!",
+//             tabBarIcon: ({ focused, color, size }) => {
+//               return (
+//                 <Image
+//                   source={require("./app/images/icon-decision.png")}
+//                   style={{ width: 32, height: 32, tintColor: "black" }}
+//                 />
+//               );
+//             },
+//           }}
+//         />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+//====================================
+//====================================
+//====================================
+//====================================
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  Image,
+} from "react-native";
+
+import PeopleScreen from "./app/screens/PeopleScreen";
+import DecisionScreen from "./app/screens/DecisionScreen";
+import RestaurantsScreen from "./app/screens/RestaurantsScreen";
+// TabNavigator;
+import { Constants } from "expo";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+// import Ionicons from "react-native-vector-icons/Ionicons";
+import Ionicons from "@expo/vector-icons/Ionicons";
+// import { TabNavigator } from "react-navigation";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+console.log("------------------------------------------------------------");
+console.log(`RestaurantChooser starting on ${Platform.OS}`);
+
+const platformOS = Platform.OS.toLowerCase();
+
 const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
+  // const logos = {
+  //   peopleLogo: require("./app/images/icon-people.png"),
+  //   decisionLogo: require("./app/images/icon-decision.png"),
+  //   restaurantsLogo: require("./app/images/icon-restaurants.png"),
+  // };
+
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        <Tab.Navigator>
-          {/* <Tab.Screen name="Home" component={HomeScreen} /> */}
-          {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+        {/* <Image source={logoSelect("PL")} /> */}
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              // You can return any component that you like here!
+              return (
+                <Image
+                  source={require("./app/images/icon-people.png")}
+                  style={{ width: 32, height: 32, tintColor: "black" }}
+                />
+              );
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+          }}
+        >
           <Tab.Screen name="People" component={PeopleScreen} />
-          <Tab.Screen name="Decision" component={DecisionScreen} />
+          <Tab.Screen
+            name="Decision"
+            component={DecisionScreen}
+            // options={{
+
+            //   tabBarLabel: "12!",
+            //   tabBarIcon: ({ focused, color, size }) => {
+            //     return (
+            //       <Image
+            //         source={require("./app/images/icon-decision.png")}
+            //         style={{ width: 32, height: 32, tintColor: "black" }}
+            //       />
+            //     );
+            //   },
+            // }}
+          />
           <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
         </Tab.Navigator>
       </NavigationContainer>
