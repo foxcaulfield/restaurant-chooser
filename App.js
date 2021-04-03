ea71bacc5f4bc3eddc1f650b8f978726fb4d819c
Fox Caulfield <mrcoderprog@gmail.com>
2021-04-03 18:15:46 +0300

    Add custom components

--- App.js
+++ App.js
@@ -1,99 +1,3 @@
-// //================================================================================
-// // You can import Ionicons from @expo/vector-icons if you use Expo or
-// // react-native-vector-icons/Ionicons otherwise.
-
-// //
-// import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-// //
-
-// import * as React from "react";
-// import { Text, View, Image } from "react-native";
-// import { Ionicons } from "@expo/vector-icons";
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import { NavigationContainer } from "@react-navigation/native";
-
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>Home!</Text>
-//     </View>
-//   );
-// }
-
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
-
-// const Tab = createBottomTabNavigator();
-
-// //
-// // const Tab = createMaterialTopTabNavigator();
-// //
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator
-//         screenOptions={({ route }) => ({
-//           tabBarIcon: ({ focused, color, size }) => {
-//             // You can return any component that you like here!
-//             return (
-//               <Image
-//                 source={require("./app/images/icon-people.png")}
-//                 style={{ width: 32, height: 32, tintColor: "black" }}
-//               />
-//             );
-//           },
-//         })}
-//         tabBarOptions={{
-//           activeTintColor: "tomato",
-//           inactiveTintColor: "gray",
-//         }}
-//       >
-//         <Tab.Screen
-//           name="Home"
-//           component={HomeScreen}
-//           options={{
-//             tabBarLabel: "Home!",
-//             tabBarIcon: ({ focused, color, size }) => {
-//               return (
-//                 <Image
-//                   source={require("./app/images/icon-restaurants.png")}
-//                   style={{ width: 32, height: 32, tintColor: "black" }}
-//                 />
-//               );
-//             },
-//           }}
-//         />
-
-//         <Tab.Screen
-//           name="Settings"
-//           component={SettingsScreen}
-//           options={{
-//             tabBarLabel: "12!",
-//             tabBarIcon: ({ focused, color, size }) => {
-//               return (
-//                 <Image
-//                   source={require("./app/images/icon-decision.png")}
-//                   style={{ width: 32, height: 32, tintColor: "black" }}
-//                 />
-//               );
-//             },
-//           }}
-//         />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-//====================================
-//====================================
-//====================================
-//====================================
 import React, { useState } from "react";
 import {
   Text,
@@ -109,15 +13,12 @@ import {
 import PeopleScreen from "./app/screens/PeopleScreen";
 import DecisionScreen from "./app/screens/DecisionScreen";
 import RestaurantsScreen from "./app/screens/RestaurantsScreen";
-// TabNavigator;
 import { Constants } from "expo";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-// import Ionicons from "react-native-vector-icons/Ionicons";
-import Ionicons from "@expo/vector-icons/Ionicons";
-// import { TabNavigator } from "react-navigation";
+// import Ionicons from "@expo/vector-icons/Ionicons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 console.log("------------------------------------------------------------");
 console.log(`RestaurantChooser starting on ${Platform.OS}`);
@@ -127,51 +28,59 @@ const platformOS = Platform.OS.toLowerCase();
 const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
-  // const logos = {
-  //   peopleLogo: require("./app/images/icon-people.png"),
-  //   decisionLogo: require("./app/images/icon-decision.png"),
-  //   restaurantsLogo: require("./app/images/icon-restaurants.png"),
-  // };
-
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        {/* <Image source={logoSelect("PL")} /> */}
         <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              // You can return any component that you like here!
-              return (
-                <Image
-                  source={require("./app/images/icon-people.png")}
-                  style={{ width: 32, height: 32, tintColor: "black" }}
-                />
-              );
-            },
-          })}
+          initialRouteName="Decision"
+          backBehavior="initialRoute"
+          tabBarPosition="bottom"
           tabBarOptions={{
-            activeTintColor: "tomato",
+            activeTintColor: "black",
             inactiveTintColor: "gray",
+            lazy: true,
+            showIcon: true,
           }}
         >
-          <Tab.Screen name="People" component={PeopleScreen} />
+          <Tab.Screen
+            name="People"
+            component={PeopleScreen}
+            options={{
+              tabBarIcon: () => (
+                <MaterialCommunityIcons
+                  name="account-group"
+                  size={24}
+                  color="black"
+                />
+              ),
+            }}
+          />
           <Tab.Screen
             name="Decision"
             component={DecisionScreen}
-            // options={{
-
-            //   tabBarLabel: "12!",
-            //   tabBarIcon: ({ focused, color, size }) => {
-            //     return (
-            //       <Image
-            //         source={require("./app/images/icon-decision.png")}
-            //         style={{ width: 32, height: 32, tintColor: "black" }}
-            //       />
-            //     );
-            //   },
-            // }}
+            options={{
+              tabBarIcon: () => (
+                <MaterialCommunityIcons
+                  name="transit-connection-variant"
+                  size={24}
+                  color="black"
+                />
+              ),
+            }}
           />
-          <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Screen
+            name="Restaurants"
+            component={RestaurantsScreen}
+            options={{
+              tabBarIcon: () => (
+                <MaterialCommunityIcons
+                  name="silverware-fork-knife"
+                  size={24}
+                  color="black"
+                />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
--- app/screens/RestaurantsScreen.js
+++ app/screens/RestaurantsScreen.js
@@ -1,12 +1,271 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import CustomButton from "../components/CustomButton";
+import CustomTextInput from "../components/CustomTextInput";
+import {
+  Alert,
+  BackHandler,
+  FlatList,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Root, Toast } from "native-base";
 
-function RestaurantsScreen(props) {
+import { Constants } from "expo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { Picker } from "@react-native-picker/picker";
+import { useIsFocused } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const STORAGE_KEY = "@save_restaurant";
+
+function AddScreen({ navigation }) {
+  const [restaurant, setRestaurant] = useState([]);
+
+  useEffect(() => {
+    readData();
+  }, []);
+
+  // read data
+  const readData = async () => {
+    try {
+      const restaurant = await AsyncStorage.getItem(STORAGE_KEY);
+      if (restaurant !== null) {
+        const restaurantParsed = JSON.parse(restaurant);
+        setRestaurant(restaurantParsed);
+      }
+    } catch (e) {
+      alert("Failed to fetch the data from storage12");
+    }
+  };
+
+  // save data
+
+  const saveData = async () => {
+    try {
+      //   const restaurantStringify = JSON.stringify(restaurant);
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(restaurant));
+      setRestaurant(restaurant);
+      alert("Data successfully saved");
+    } catch (e) {
+      alert("Failed to save the data to the storage2");
+    }
+  };
+
+  const clearStorage = async () => {
+    try {
+      await AsyncStorage.clear();
+      alert("Storage successfully cleared!");
+    } catch (e) {
+      alert("Failed to clear the async storage3");
+    }
+  };
+
+  const onChangeText = (restaurant) => setRestaurant(restaurant);
+
+  const onSubmitEditing = () => {
+    if (!restaurant) return;
+    saveData(restaurant);
+    setRestaurant("");
+  };
+
   return (
-    <View>
-      <Text>Restaurants</Text>
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+      <View style={styles.panel}>
+        <TextInput
+          style={styles.input}
+          value={restaurant}
+          placeholder="ENTER RESTAURANT HERE!!!"
+          onChangeText={onChangeText}
+          onSubmitEditing={onSubmitEditing}
+        />
+        {/* <Text style={styles.text}>Your age is {restaurant}</Text> */}
+        {/* <TouchableOpacity onPress={clearStorage} style={styles.button}>
+          <Text style={styles.buttonText}>Clear Storage</Text>
+        </TouchableOpacity> */}
+      </View>
     </View>
   );
 }
 
+function ListScreen({ navigation }) {
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    readData();
+    //Update the state you want to be updated
+  }, [isFocused]);
+  const [restaurant, setRestaurant] = useState({
+    firstName: "",
+    lastName: "sdf",
+  });
+
+  useEffect(() => {
+    readData();
+  }, []);
+
+  // read data
+  const readData = async () => {
+    try {
+      const restaurant = await AsyncStorage.getItem(STORAGE_KEY);
+      if (restaurant !== null) {
+        const restaurantParsed = JSON.parse(restaurant);
+        setRestaurant(restaurantParsed);
+      }
+    } catch (e) {
+      debugger;
+      alert("Failed to fetch the data from storage12s2");
+      alert(e);
+    }
+  };
+
+  // save data
+
+  const saveData = async () => {
+    try {
+      //   const restaurantStringify = JSON.stringify(restaurant);
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(restaurant));
+      setRestaurant(restaurant);
+      alert("Data successfully saved");
+    } catch (e) {
+      alert("Failed to save the data to the storage2");
+    }
+  };
+
+  const clearStorage = async () => {
+    try {
+      await AsyncStorage.clear();
+      alert("Storage successfully cleared!");
+    } catch (e) {
+      alert("Failed to clear the async storage.");
+    }
+  };
+
+  const onChangeText = (restaurant) => setRestaurant(restaurant);
+
+  const onSubmitEditing = () => {
+    if (!restaurant) return;
+    saveData(restaurant);
+    setRestaurant("");
+  };
+  return (
+    <Root>
+      <View style={styles.container}>
+        <View style={styles.header}></View>
+        <View style={styles.panel}>
+          <Text style={styles.text}>Restaurant IS {restaurant}</Text>
+          <TouchableOpacity onPress={clearStorage} style={styles.button}>
+            <Text style={styles.buttonText}>Clear Storage</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.listScreenContainer}>
+        <CustomButton
+          text="Add Restaurant"
+          width="94%"
+          onPress={() => {
+            navigation.navigate("AddScreen");
+          }}
+        />
+      </View>
+    </Root>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function RestaurantsScreen(props) {
+  return (
+    <Stack.Navigator initialRouteName="ListScreen" backBehavior="initialRoute">
+      <Stack.Screen name="AddScreen" component={AddScreen} />
+      <Stack.Screen name="ListScreen" component={ListScreen} />
+    </Stack.Navigator>
+  );
+}
+
 export default RestaurantsScreen;
+
+const styles = StyleSheet.create({
+  listScreenContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  restaurantList: {
+    width: "94%",
+  },
+
+  restaurantContainer: {
+    flexDirection: "row",
+    marginTop: 4,
+    marginBottom: 4,
+    borderColor: "#e0e0e0",
+    borderBottomWidth: 2,
+    alignItems: "center",
+  },
+
+  restaurantName: {
+    flex: 1,
+  },
+
+  addScreenContainer: {
+    // marginTop: Constants.statusBarHeight,
+  },
+
+  addScreenInnerContainer: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 20,
+    width: "100%",
+  },
+
+  addScreenFormContainer: {
+    width: "96%",
+  },
+
+  fieldLabel: {
+    marginLeft: 10,
+  },
+
+  pickerContainer: {
+    ...Platform.select({
+      ios: {},
+      android: {
+        width: "96%",
+        borderRadius: 8,
+        borderColor: "#c0c0c0",
+        borderWidth: 2,
+        marginLeft: 10,
+        marginBottom: 20,
+        marginTop: 4,
+      },
+    }),
+  },
+
+  picker: {
+    ...Platform.select({
+      ios: {
+        width: "96%",
+        borderRadius: 8,
+        borderColor: "#c0c0c0",
+        borderWidth: 2,
+        marginLeft: 10,
+        marginBottom: 20,
+        marginTop: 4,
+      },
+      android: {},
+    }),
+  },
+
+  addScreenButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+});
